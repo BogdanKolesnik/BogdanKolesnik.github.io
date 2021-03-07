f72b2c5ded15ce72842c801bfc0681a7fd938959
Bogdan_Kolesnik <53654260+BogdanKolesnik@users.noreply.github.com>
2021-03-07 17:06:03 +0200

    Changes #0
    Uploading files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Wneh click burger button, adding class .active 
+$(document).ready(function() {
+  $('.header__nav-menu__burger-button').click(function(event) {
+    $('.header__nav-menu__burger-button, .header__burger-nav-menu, .header__nav-menu__burger__logo').toggleClass('active');
+    $('body').toggleClass('lock-scroll');
+  });
+});
+
+// If screen size > 991, removing class .active
+$(window).resize(function() {
+  var sWidth = $( window ).width();
+  if(sWidth > 991) {
+    $('.header__nav-menu__burger-button, .header__burger-nav-menu, .header__nav-menu__burger__logo').removeClass('active');
+  }
+});
