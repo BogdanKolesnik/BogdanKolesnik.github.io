925d30b44b7a9532f2db314fa20a043c56d1354d
Bogdan_Kolesnik <53654260+BogdanKolesnik@users.noreply.github.com>
2021-03-09 23:49:16 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,5 +11,6 @@ $(window).resize(function() {
   var sWidth = $( window ).width();
   if(sWidth > 991) {
     $('.header__nav-menu__burger-button, .header__burger-nav-menu, .header__nav-menu__burger__logo').removeClass('active');
+    $('body').removeClass('lock-scroll');
   }
 });
